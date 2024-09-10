d7aff44b4dc4d6bb70ca19f20b5dae5bbca82837
Dileep <ddr.dileepraloti@gmail.com>
2024-09-10 13:10:44 +0530

    fix: user registration controller

--- src/controllers/user.controllers.ts
+++ src/controllers/user.controllers.ts
@@ -1,14 +1,20 @@
 import { Request, Response } from "express";
 import apiResponse from "../utils/api.response";
 import UserModal from "../models/user.models";
+import { hashPassword } from "../utils/bcrypt";
 
 export const registerUserController = async (req: Request, res: Response) => {
   try {
-    const user = new UserModal(req.body);
+    const user = new UserModal({
+      ...req.body,
+      password: await hashPassword(req.body.password),
+    });
     await user.save();
 
-    res.json(apiResponse.SUCCESS({ user }, "User registered successfully"));
+    res.json(
+      apiResponse.SUCCESS({ user: req.body }, "User registered successfully")
+    );
   } catch (error) {
-    apiResponse.OTHER(error);
+    return res.status(400).json(apiResponse.OTHER(error));
   }
 };
