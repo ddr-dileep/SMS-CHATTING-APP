5c3357712683f18341515afd713dafc887e7ffe3
Dileep <ddr.dileepraloti@gmail.com>
2024-09-10 13:10:07 +0530

    fix : db connection

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,9 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import rootRouter from "./routers/router";
-dotenv.config();
+import databaseConfig from "./configs/db.config";
 
+dotenv.config();
 const app = express();
 
 app.use(cors());
@@ -13,5 +14,6 @@ app.use("/api/v1", rootRouter);
 const port = process.env.PORT! || 3000;
 
 app.listen(port, () => {
+  databaseConfig();
   console.log(`Server is running on http://localhost:${port}`);
 });
