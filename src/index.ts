55d39dfdd69323452aad890ae329fadc2019c2f0
Dileep <ddr.dileepraloti@gmail.com>
2024-09-10 12:12:53 +0530

    feat : added root router

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import rootRouter from "./routers/router";
 dotenv.config();
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use("/api/v1", rootRouter);
 
 const port = process.env.PORT! || 3000;
 
