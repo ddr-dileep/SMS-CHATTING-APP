b5f5663d6ea95987fc1d447b5d794cf048f919bc
Dileep <ddr.dileepraloti@gmail.com>
2024-09-18 16:47:53 +0530

    feat : message api

--- src/controllers/message.controllers.ts
+++ src/controllers/message.controllers.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import apiResponse from "../utils/api.response";
 import messageModel from "../models/message.models";
+import chatModel from "../models/chat.models";
 
 export const sendMessageController = async (
   req: Request | any,
@@ -9,14 +10,125 @@ export const sendMessageController = async (
   try {
     const sender = req.user._id;
     const { content, chat } = req.body;
+    const validChat = await chatModel.findById(chat);
+    if (!validChat) {
+      return res
+        .status(400)
+        .json(apiResponse.ERROR("invalid_chat", "Invalid chat"));
+    }
     const message = new messageModel({
       sender,
       content,
       chat,
     });
 
+    await message.save();
+
+    await message.populate("sender", "username email profilePicture _id");
+    await message.populate("chat");
+
     return res.json(
       apiResponse.SUCCESS({ message }, "Message sent successfully")
+    );
+  } catch (error) {
+    return res.status(400).json(apiResponse.OTHER(error));
+  }
+};
+
+export const getAllmessagesController = async (
+  req: Request | any,
+  res: Response
+) => {
+  try {
+    const chatId = req.params.chatId;
+    const messages = await messageModel
+      .find({ chat: chatId })
+      .populate("sender", "username profilePicture _id")
+      .select("-__v -updatedAt")
+      .sort({ createdAt: -1 });
+
+    return res.json(
+      apiResponse.SUCCESS(
+        { count: messages.length, messages },
+        "Messages retrieved successfully"
+      )
+    );
+  } catch (error) {
+    return res.status(400).json(apiResponse.OTHER(error));
+  }
+};
+
+export const deleteMessageController = async (
+  req: Request | any,
+  res: Response
+) => {
+  try {
+    const { messageId } = req.params;
+    const message = await messageModel.findByIdAndDelete(messageId);
+
+    if (!message) {
+      return res
+        .status(404)
+        .json(apiResponse.ERROR("not_found", "Message not found"));
+    }
+
+    return res.json(
+      apiResponse.SUCCESS({ message }, "Message deleted successfully")
+    );
+  } catch (error) {
+    return res.status(400).json(apiResponse.OTHER(error));
+  }
+};
+
+export const updateMessageController = async (
+  req: Request | any,
+  res: Response
+) => {
+  try {
+    const { messageId } = req.params;
+    const { content } = req.body;
+    const message = await messageModel.findByIdAndUpdate(
+      messageId,
+      { content },
+      { new: true }
+    );
+
+    if (!message) {
+      return res
+        .status(404)
+        .json(apiResponse.ERROR("not_found", "Message not found"));
+    }
+
+    await message.populate("sender", "username profilePicture _id");
+    await message.populate("chat");
+
+    return res.json(
+      apiResponse.SUCCESS({ message }, "Message updated successfully")
+    );
+  } catch (error) {
+    return res.status(400).json(apiResponse.OTHER(error));
+  }
+};
+
+export const getOneMessageController = async (
+  req: Request | any,
+  res: Response
+) => {
+  try {
+    const { messageId } = req.params;
+    const message = await messageModel
+      .findById(messageId)
+      .populate("sender", "username profilePicture _id")
+      .populate("chat");
+
+    if (!message) {
+      return res
+        .status(404)
+        .json(apiResponse.ERROR("not_found", "Message not found"));
+    }
+
+    return res.json(
+      apiResponse.SUCCESS({ message }, "Message retrieved successfully")
     );
   } catch (error) {
     return res.status(400).json(apiResponse.OTHER(error));
--- src/routers/message.router.ts
+++ src/routers/message.router.ts
@@ -1,9 +1,35 @@
 import { Router } from "express";
-import { sendMessageController } from "../controllers/message.controllers";
+import {
+  deleteMessageController,
+  getAllmessagesController,
+  getOneMessageController,
+  sendMessageController,
+  updateMessageController,
+} from "../controllers/message.controllers";
 import { authTokenMiddleware } from "../utils/token";
 
 const messageRouter = Router();
 
 messageRouter.post("/send-message", authTokenMiddleware, sendMessageController);
+messageRouter.get(
+  "/get-chat-messages/:chatId",
+  authTokenMiddleware,
+  getAllmessagesController
+);
+messageRouter.delete(
+  "/delete-message/:messageId",
+  authTokenMiddleware,
+  deleteMessageController
+);
+messageRouter.patch(
+  "/update-chat-messages/:messageId",
+  authTokenMiddleware,
+  updateMessageController
+);
+messageRouter.get(
+  "/get-one-message/:messageId",
+  authTokenMiddleware,
+  getOneMessageController
+);
 
 export default messageRouter;
