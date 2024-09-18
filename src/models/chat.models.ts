dfece534a07583bc1aafb5e2d501ea6d3cbe2b8d
Dileep <ddr.dileepraloti@gmail.com>
2024-09-18 12:04:38 +0530

    fix : chat model added user count

--- src/models/chat.models.ts
+++ src/models/chat.models.ts
@@ -1,7 +1,7 @@
 import { Document, model, Schema } from "mongoose";
 import { Iuser } from "./user.models";
 
-const chatSchema: Schema = new Schema(
+const chatSchema: Schema<IChat> = new Schema(
   {
     name: { type: String, trim: true, required: true },
     isGroupChat: { type: Boolean, default: false },
@@ -23,6 +23,14 @@ const chatSchema: Schema = new Schema(
   { timestamps: true }
 );
 
+// Add a virtual field for usercount
+chatSchema.virtual("user-count").get(function () {
+  return this.users.length;
+});
+
+// Ensure virtuals are included when converting documents to JSON
+chatSchema.set("toJSON", { virtuals: true });
+
 const chatModel = model<IChat>("Chat", chatSchema);
 export default chatModel;
 
@@ -30,6 +38,7 @@ export interface IChat extends Document {
   name: string;
   isGroupChat: boolean;
   users: Iuser[];
+  usercount: number;
   latestMessage: any;
   groupAdmin: Iuser;
 }
