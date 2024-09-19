d662e73ab359bb7c3143994634b5e2b4d366688e
Dileep <ddr.dileepraloti@gmail.com>
2024-09-19 16:58:51 +0530

    feat : comment model for blog

---
+++ src/models/comment.model.ts
@@ -0,0 +1,27 @@
+import { Document } from "mongoose";
+import { model, Schema } from "mongoose";
+import { Iuser } from "./user.models";
+import { IBlog } from "./blog.models";
+
+const commentSchema: Schema = new Schema(
+  {
+    content: { type: String, required: true },
+    author: { type: Schema.Types.ObjectId, ref: "User", required: true },
+    blog: { type: Schema.Types.ObjectId, ref: "Blog", required: true },
+    createdAt: { type: Date, default: Date.now },
+    replies: [{ type: Schema.Types.ObjectId, ref: "Comment" }],
+    likes: [{ type: Schema.Types.ObjectId, ref: "User" }],
+  },
+  { timestamps: true }
+);
+
+const commentModel = model<IComment>("Comment", commentSchema);
+export default commentModel;
+
+interface IComment extends Document {
+  content: string;
+  author: Iuser;
+  blog: IBlog;
+  createdAt: Date;
+  replies: IComment[];
+}
